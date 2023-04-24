d0f5d641f8305c8042626705c89ae5f98d207ab3
Roberto Russo <robertorusso.bob@gmail.com>
2023-04-24 09:08:25 +0000

    feat: add common.sh

---
+++ common.sh
@@ -0,0 +1,12 @@
+export DEMO_SLEEP=${DEMO_SLEEP-0}
+
+print () {
+  YELLOW='\033[0;93m'
+  RED='\033[0;31m'
+  BRIGHT_RED='\033[0;91m'
+  BRIGHT_BLUE='\033[0;94m'
+  COLOR=$BRIGHT_BLUE
+  NC='\033[0m' # No Color
+  printf "${BRIGHT_RED}>>> ${COLOR}$1${NC}\n";
+}
+
